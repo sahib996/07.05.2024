e85bb744d753a138c682214d350a6a83faa3563b
sahib memmedov <sahibmemmedov7890@gmail.com>
2024-05-06 16:13:31 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pronia.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task <IActionResult> Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Contanct()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Shop()
+        {
+            return View();
+        }
+        public async Task<IActionResult> About ()
+        {
+            return View();
+        }
+    }
+}
